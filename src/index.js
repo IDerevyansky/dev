239d2d5596c6b0bce8f766da4f327645ac4472af
IDerevyansky <IDerevyansky@yandex.ru>
2022-01-03 17:01:03 +0300

    -

---
+++ src/index.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
+import {
+  HashRouter,
+  Routes,
+  Route
+} from "react-router-dom";
+import Expenses from "./routes/expenses";
+import Invoices from "./routes/invoices";
+import Invoice from "./routes/invoice";
+
+
+ReactDOM.render(
+
+  <HashRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route path="expenses" element={<Expenses />} />
+          <Route path="invoices" element={<Invoices />}>
+          <Route
+        index
+        element={
+          <main style={{ padding: "1rem" }}>
+            <p>Select an invoice</p>
+          </main>
+        }
+      />
+            <Route path=":invoiceId" element={<Invoice />} />
+          </Route>
+
+        <Route
+          path="*"
+          element={
+            <main style={{ padding: "1rem" }}>
+              <p>There's nothing here!</p>
+            </main>
+          }
+        />
+
+      </Route>
+    </Routes>
+   </HashRouter>,
+  document.getElementById('root')
+
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
+
+
