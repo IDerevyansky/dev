76d0cb62e66cf54338bf9284e70a4ed0a69261c4
IDerevyansky <IDerevyansky@yandex.ru>
2022-02-07 01:15:07 +0300

    -

--- src/components/body/pages/main/Main.js
+++ src/components/body/pages/main/Main.js
@@ -5,24 +5,33 @@ import './Main.css';
 function Main(){
 
     return(
-        <div className="main_banners_container">
-            
-            <div className="banners_box">
+        <>
 
-                <div className="text_container">
-                    <h1 className="h1">Вам достаточно знать номер телефона данного человека. </h1>
-                    <p className="p">
-                        Вам достаточно знать номер телефона данного человека, и вы можете создать 
-                        ссылку, которая позволит начать с ним чат. Вам достаточно знать номер 
-                        телефона данного человека, и вы можете создать ссылку, которая позволит 
-                        начать с ним чат. 
-                    </p>
+            <div className="main_banners_container">
+                
+                <div className="banners_box">
+
+                    <div className="text_container">
+                        <h1 className="h1">Вам достаточно знать номер телефона данного человека. </h1>
+                        <p className="p">
+                            Вам достаточно знать номер телефона данного человека, и вы можете создать 
+                            ссылку, которая позволит начать с ним чат. Вам достаточно знать номер 
+                            телефона данного человека, и вы можете создать ссылку, которая позволит 
+                            начать с ним чат. 
+                        </p>
+                        <div className="box_btn"></div>
+                    </div>
+
                 </div>
-
-            </div>
+                
             
-           
-        </div>
+            </div>
+
+            <div className="two_block">
+                <div className="test_content">sdfsdfs</div>
+            </div>
+
+        </>    
     );
 
 }
