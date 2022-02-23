04f9e820abfcba734b6a2a3c59f90ebc21cefa15
IDerevyansky <IDerevyansky@yandex.ru>
2022-02-24 00:53:05 +0300

    -

--- src/components/body/pages/main/MainV2.js
+++ src/components/body/pages/main/MainV2.js
@@ -7,25 +7,42 @@ function MainV2(){
 
     return(
 
-        <div className="main-container">
-            <div className="side Left">
-                <div className="Grad"></div>
-              </div>
-              <div className="side Right">
+        <>
 
-                <div className="text-container">
-                  <h1>Вам достаточно знать номер телефона данного человека.</h1>
-                  <p>
-                    Вам достаточно знать номер телефона данного человека, и вы можете создать 
-                    ссылку, которая позволит начать с ним чат. Вам достаточно знать номер 
-                    телефона данного человека, и вы можете создать ссылку, которая позволит 
-                    начать с ним чат. 
-                  </p>
-                </div>  
-              
-                <CallBackLarge/>
+              <div className="main-container">
+                  <div className="side Left">
+                      <div className="Grad"></div>
+                    </div>
+                    <div className="side Right">
+
+                      <div className="text-container">
+                        <h1>Вам достаточно знать номер телефона данного человека.</h1>
+                        <p>
+                          Вам достаточно знать номер телефона данного человека, и вы можете создать 
+                          ссылку, которая позволит начать с ним чат. Вам достаточно знать номер 
+                          телефона данного человека, и вы можете создать ссылку, которая позволит 
+                          начать с ним чат. 
+                        </p>
+                      </div>  
+                    
+                      <CallBackLarge/>
+
+                    </div>
               </div>
-        </div>
+
+
+              <div className="container-general">
+            
+                <div className="two-block">
+                  <div className="two-block_left">qweqwe</div>
+                  <div className="two-block_right">qweqwe</div>
+                </div>
+
+              </div>
+
+
+
+        </>        
 
     );
 
