dc8af3ae67eb32a878f4871cb6e0a4b0e6a87dd5
IDerevyansky <igor.derevyansky@gmail.com>
2022-05-31 00:09:15 +0300

    -

--- src/components/topBanner/topBanner.js
+++ src/components/topBanner/topBanner.js
@@ -4,12 +4,14 @@ import './topBanner.css';
 function TopBanner(props){
     return(
 
+
     <div 
         className="top-banner" 
         style={{backgroundImage:'url('+props.img+')'}}
     >
 
     </div>
+    
 
     )
 }
