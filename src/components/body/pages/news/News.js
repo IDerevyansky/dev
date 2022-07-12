f228d6ecff5a9d2f6c8dd40fd8d73261d3f7e31e
IDerevyansky <igor.derevyansky@gmail.com>
2022-07-13 00:32:16 +0300

    -

--- src/components/body/pages/news/News.js
+++ src/components/body/pages/news/News.js
@@ -7,6 +7,89 @@ import Back from "../../../back/back";
 import NewsTreeBlock from "./newsThreeBlock";
 
 function News(){
+
+let data = {
+    "news":[
+        {
+            "id":1,
+            "img":"",
+            "title":"Title1",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":2,
+            "img":"",
+            "title":"Title2",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":3,
+            "img":"",
+            "title":"Title3",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":4,
+            "img":"",
+            "title":"Title4",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":5,
+            "img":"",
+            "title":"Title5",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":6,
+            "img":"",
+            "title":"Title6",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":7,
+            "img":"",
+            "title":"Title7",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":8,
+            "img":"",
+            "title":"Title8",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":9,
+            "img":"",
+            "title":"Title9",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+        {
+            "id":10,
+            "img":"",
+            "title":"Title10",
+            "litleText":"Text text text",
+            "date":"12.34.2022"
+        },
+    ]
+}
+
+const result = [];
+
+for (let index = 0; index <= 6; index++) {
+    result[index] = data.news[index];
+}
+// console.log(result);
+
     return(
         
     <>
@@ -17,13 +100,18 @@ function News(){
         
         <div className="container-general">
             <div className="block-top">
-                
+
                 <div className="adpt_m">
                     <Back location='ru'/>
                     <h1 className="mb-[24px]" dangerouslySetInnerHTML={{__html:'Мои новости'}}></h1>
                 </div>
                 
-                <NewsTreeBlock/>
+                <NewsTreeBlock data={result}/>
+
+                <div className="adpt_m">
+                    
+                </div>
+
             </div>
         </div>
    
--- src/components/body/pages/news/newsThreeBlock.js
+++ src/components/body/pages/news/newsThreeBlock.js
@@ -4,18 +4,131 @@ function NewsTreeBlock(props){
 
     let gradientNewsCard = "linear-gradient(180deg, rgba(42, 46, 59, 0) 0%, #2A2E3B 100%)";
 
+    let newsContent = props.data.map((q, w, e)=>{
+        console.log(q);
+    });
+
     return(
+
         <>
             <div className="scroll-pl-6 snap-x newsBlock adpt_m">
-                <div className="snap-center gardNews block-2-3" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}></div>
-                <div className="snap-center gardNews block-1-3"></div>
-                <div className="snap-center gardNews block-1-3"></div>
-                <div className="snap-center gardNews block-2-3"></div>
-                <div className="snap-center gardNews"></div>
-                <div className="snap-center gardNews"></div>
-                <div className="snap-center gardNews"></div>
+
+
+
+                <div className="snap-center cardNews block-2-3" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+                
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews block-1-3" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews block-1-3" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews block-2-3" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
+
+                <div className="snap-center cardNews" style={{background:gradientNewsCard + ", url(https://images.unsplash.com/photo-1603482594758-76e1fee40ff4?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1740&q=80) center, center"}}>
+
+                    <div className="cardNewsContent">
+
+                        <div className="cardNewsContentHead">
+                            <h2 className="h2-Two-Line" dangerouslySetInnerHTML={{__html:"Title wery loooong"}}></h2>
+                            <p className="p-Three-Line" dangerouslySetInnerHTML={{__html:"Text text text text text text text text text text"}}></p>
+                        </div>
+                        <p className="text-[#F7F5EF]" dangerouslySetInnerHTML={{__html:"12.07.2022"}}></p>
+
+                    </div>
+
+                </div>
+
+
             </div>
         </>
+
     );
 }
 
